ebf9db68556914f9c74f6c7a8725b9ff46dfd71e
Williams Malachy <billmal071@gmail.com>
2025-12-18 23:24:35 +0100

    ci: deploy app

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'fetchit-be',
+      script: 'dist/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+      error_file: './logs/pm2-error.log',
+      out_file: './logs/pm2-out.log',
+      log_file: './logs/pm2-combined.log',
+      time: true,
+      merge_logs: true,
+    },
+  ],
+};
