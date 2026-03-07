7f3034a640100c89404c70b38328f7ea2a603205
Williams Malachy <billmal071@gmail.com>
2026-03-07 23:13:38 +0100

    style: apply prettier formatting, remove erroneous mkdir in CI

--- src/common/utils/hash.util.ts
+++ src/common/utils/hash.util.ts
@@ -6,10 +6,7 @@ export async function hashPassword(password: string): Promise<string> {
   return bcrypt.hash(password, SALT_ROUNDS);
 }
 
-export async function comparePassword(
-  password: string,
-  hashedPassword: string,
-): Promise<boolean> {
+export async function comparePassword(password: string, hashedPassword: string): Promise<boolean> {
   return bcrypt.compare(password, hashedPassword);
 }
 
